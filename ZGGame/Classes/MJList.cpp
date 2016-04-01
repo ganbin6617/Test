20f4683dd400bf6bdc2fea46fe8f33a45e97311e
ganbin6617 <ganbin_136079817@qq.com>
2016-04-01 18:18:03 +0800

    addBJ

---
+++ ZGGame/Classes/MJList.cpp
@@ -0,0 +1,9 @@
+//
+//  MJList.cpp
+//  ZGGame
+//
+//  Created by Thesy on 16/3/16.
+//
+//
+
+#include "MJList.h"
---
+++ ZGGame/Classes/MJList.h
@@ -0,0 +1,14 @@
+//
+//  MJList.h
+//  ZGGame
+//
+//  Created by Thesy on 16/3/16.
+//
+//
+
+#ifndef __ZGGame__MJList__
+#define __ZGGame__MJList__
+
+#include <stdio.h>
+
+#endif /* defined(__ZGGame__MJList__) */
